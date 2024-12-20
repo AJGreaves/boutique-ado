18a387d67df0e2530f33a0e6a647a55e54a3808a
AJGreaves <anna.j.greaves@gmail.com>
2024-12-20 02:23:58 +0100

    Hide secret key and create requirements.txt

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d!c!s&5^szc7w-7$2nkn5c7x+drw-n#wu--shv0&n=%d4=&xr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
