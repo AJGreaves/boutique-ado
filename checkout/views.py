9c03a2af38f92b5d3ac71df4025b3ce4369a9dc3
AJGreaves <anna.j.greaves@gmail.com>
2025-01-10 11:53:13 +0100

    Stripe Payments > Stripe Part 14 - Add checkout form data caching in payment intent - CORRECT CODE

--- checkout/views.py
+++ checkout/views.py
@@ -23,8 +23,8 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be processed right now. \
-            Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -47,7 +47,6 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
@@ -79,11 +78,9 @@ def checkout(request):
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
-        
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
-            
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -101,18 +98,18 @@ def checkout(request):
 
         order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
 
 
 def checkout_success(request, order_number):
