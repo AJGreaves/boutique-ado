1ab1cd6ad2eed921f472b4551b453ccae6fa3684
AJGreaves <anna.j.greaves@gmail.com>
2025-01-27 11:54:31 +0100

    Code at start of "Testing our first webhook"

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # local preview
+]
 
 # Application definition
 
@@ -182,3 +183,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
